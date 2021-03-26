baf6a2d09da452866a638e61831549264c0892c9
Julie Van Kirk <jvankirk@iastate.edu>
2021-03-26 09:08:14 -0500

    add login tests

--- frontend/src/pages/Login.test.js
+++ frontend/src/pages/Login.test.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { render, cleanup } from '@testing-library/react';
 import { act } from 'react-dom/test-utils';
 import Login from './Login';
+import { useHistory } from 'react-router-dom';
 
 let container;
 
@@ -16,11 +17,33 @@ afterEach(() => {
     container = null;
 });
 
-it('can render and update a form', () => {
+it('can render The Suite Sweet on Login', () => {
     act(() => {
         ReactDOM.render(<Login/>, container);
     });
 
-    const label = container.querySelector('label');
-    expect(label.textContent).toBe('User Name');
+    const text = container.querySelector('h1');
+    expect(text.textContent).toBe('The Sweet Suite');
+});
+
+it('can render Login on Login', () => {
+    act(() => {
+        ReactDOM.render(<Login/>, container);
+    });
+
+    const text = container.querySelector('h2');
+    expect(text.textContent).toBe('Login');
 })
+
+it('can click button', () => {
+    act(() => {
+        ReactDOM.render(<Login/>, container);
+    });
+    
+    const button = container.querySelector('button');
+    expect(button.textContent).toBe("Login");
+    act(() => {
+        button.dispatchEvent(new MouseEvent('click'), {userId: "user", password: "pass" })
+    });
+});
+
