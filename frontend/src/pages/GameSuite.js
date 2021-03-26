319694b3dfe2777f78e0a34c3fb394313ec7733d
Moriah Zimmerman <moriahz@iastate.edu>
2021-03-25 22:22:33 -0500

    link to checkers page

--- frontend/src/pages/GameSuite.js
+++ frontend/src/pages/GameSuite.js
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import './GameSuite.css';
 import checkers from './images/Checkers.jpg';
 
-function loadCheckers() {
-    console.log("playing checkers");
-}
-
 // code list of props here, find out how to specify props at bottom
 const GameSuite = (props) => {
+    const history = useHistory();
+
+    const loadCheckers = () => {
+        let path = "/checkers";
+        history.push(path);
+    };
+    
     return (
         <div>
         <div className = "GameSuiteHeader">
