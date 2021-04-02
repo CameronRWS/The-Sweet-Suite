d9042952bdddd1a4cc8bc8fc1a7411871006f07d
David Bone <59885088+davidbone4@users.noreply.github.com>
2021-04-02 00:47:34 -0500

    added pieces to the checkers board

--- frontend/src/pages/games/checkers/Board.js
+++ frontend/src/pages/games/checkers/Board.js
@@ -1,16 +1,31 @@
 import React from "react";
 import "../../GameSuite.css";
 
+import redpiece from "../../../pages/images/red-checkers-piece.png";
+import blackpiece from "../../../pages/images/black-checker-piece.png";
+
 function Square(props) {
   const squareClass = props["squareClass"];
   const onClick = props["onClick"];
   const id = props["id"];
+  const img = props["img"];
+  const alt = "piece";
 
-  return (
-    <button className={squareClass} onClick={onClick} id={id}>
-      {id}
-    </button>
-  );
+  if (img) {
+    return (
+      <div>
+        <button className={squareClass} onClick={onClick} id={id}>
+          <img style={{ width: 45, height: 45 }} src={img} alt={alt} />
+        </button>
+      </div>
+    );
+  } else {
+    return (
+      <button className={squareClass} onClick={onClick} id={id}>
+        {id}
+      </button>
+    );
+  }
 }
 
 // code list of props here, find out how to specify props at bottom
@@ -28,16 +43,26 @@ const Board = (props) => {
         (col % 2 === 0 && row % 2 === 0) || (col % 2 === 1 && row % 2 === 1)
           ? "whiteSquare"
           : "blackSquare";
+      if (row === 3 || row === 4 || colorClass ==="whiteSquare") {
+        return (
+          <Square
+            squareClass={colorClass}
+            onClick={(id) => console.log(id)}
+            id={id}
+          ></Square>
+        );
+      }
+
+      const pieceColor = (row>4) ? redpiece : blackpiece;
 
       return (
         <Square
-          squareClass={colorClass}
-          onClick={(id) => console.log(id)}
-          id={id}
-        >
-          {id}
-        </Square>
-      );
+        squareClass={colorClass}
+        onClick={(id) => console.log(id)}
+        id={id}
+        img={pieceColor}
+      ></Square>
+      )
     })
   );
 
