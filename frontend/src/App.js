c639f2e4ae9cddf3cb37797a1aae313de5d82e37
Julie Van Kirk <jvankirk@iastate.edu>
2021-04-03 20:31:35 -0500

    add Auth component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,29 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Login from "./pages/Login";
 import GameSuite from "./pages/GameSuite";
 import Checkers from "./pages/Checkers";
 import Signup from "./pages/Signup";
 import Settings from "./pages/Settings";
+import Auth from "./Auth";
 
 function App() {
+  const PrivateRoute = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={ props =>
+        Auth.getAuth() ? (
+        <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: "/"
+            }}
+          />
+        )
+      }
+    />
+    );
+
   return (
     <div>
       <BrowserRouter>
