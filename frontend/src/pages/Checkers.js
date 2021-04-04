79d739d02708a3db8eb8174102326e1d0494ebfb
Moriah Zimmerman <moriahz@iastate.edu>
2021-04-03 23:27:48 -0500

    rules popup basics

--- frontend/src/pages/Checkers.js
+++ frontend/src/pages/Checkers.js
@@ -3,14 +3,28 @@ import { Link, useHistory } from "react-router-dom";
 import "./GameSuite.css";
 import "./games/checkers/Checkers.css";
 import Board from "./games/checkers/Board";
+import Modal from '../Modal';
 
 // code list of props here, find out how to specify props at bottom
 const Checkers = (props) => {
   const history = useHistory();
+  const [showModal, setShowModal] = useState(false);
 
   const loadHome = () => {
     let path = "/gamesuite";
     history.push(path);
+  };
+
+  const resetClick = () => {
+    console.log("reset");
+  };
+
+  const rulesClick = (event) => {
+    setShowModal(true);
+  };
+
+  const rulesClose = (event) => {
+    setShowModal(false);
   };
 
   return (
@@ -21,14 +35,20 @@ const Checkers = (props) => {
       <div className="GameSuiteHeader">
         <p>Checkers</p>
       </div>
-      <div className="Checkers">
-        <h3>This is a checkers board. Have fun playing!</h3>
-      </div>
+      <div className="Checkers"></div>
 
       <Board></Board>
       <div className = "Clear">
-        <button onClick={console.log("reset")} className = "Gamebutton">Reset</button>
-        <button onClick={console.log("rules")} className = "Gamebutton">Rules</button>
+        <button onClick = {resetClick} className = "Gamebutton">Reset</button>
+        <button onClick = {rulesClick} className = "Gamebutton" id = "rules">Rules</button>
+        <Modal show={showModal}>
+          <div className = "PopUp" display = "block">
+            <div className = "PopUp.internal">
+              <span onClick = {rulesClose}>&times;</span>
+              <p>Hello! Welcome to checkers!</p>
+            </div>
+         </div>
+        </Modal>
       </div>
     </div>
   );
