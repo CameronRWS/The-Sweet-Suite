7a1c01a1b6cb17eecdc3b9d824c87afdb022f8e0
David Bone <59885088+davidbone4@users.noreply.github.com>
2021-04-06 23:59:19 -0500

    changed Board from function to class

--- frontend/src/pages/Checkers.js
+++ frontend/src/pages/Checkers.js
@@ -3,7 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import "./GameSuite.css";
 import "./games/checkers/Checkers.css";
 import Board from "./games/checkers/Board";
-import Modal from '../Modal';
+import Modal from "../Modal";
 
 // code list of props here, find out how to specify props at bottom
 const Checkers = (props) => {
@@ -37,17 +37,24 @@ const Checkers = (props) => {
       </div>
       <div className="Checkers"></div>
 
-      <Board></Board>
-      <div className = "Clear">
-        <button onClick = {resetClick} className = "Gamebutton">Reset</button>
-        <button onClick = {rulesClick} className = "Gamebutton" id = "rules">Rules</button>
+      <div className="Container">
+        <Board></Board>
+      </div>
+
+      <div className="Clear">
+        <button onClick={resetClick} className="Gamebutton">
+          Reset
+        </button>
+        <button onClick={rulesClick} className="Gamebutton" id="rules">
+          Rules
+        </button>
         <Modal show={showModal}>
-          <div className = "PopUp" display = "block">
-            <div className = "PopUp.internal">
-              <span onClick = {rulesClose}>&times;</span>
+          <div className="PopUp" display="block">
+            <div className="PopUp.internal">
+              <span onClick={rulesClose}>&times;</span>
               <p>Hello! Welcome to checkers!</p>
             </div>
-         </div>
+          </div>
         </Modal>
       </div>
     </div>
--- frontend/src/pages/games/checkers/Board.js
+++ frontend/src/pages/games/checkers/Board.js
@@ -5,6 +5,10 @@ import "./Checkers.css";
 import redpiece from "../../../pages/images/red-checkers-piece.png";
 import blackpiece from "../../../pages/images/black-checker-piece.png";
 
+function onclick(id) {
+  console.log(id);
+}
+
 function Square(props) {
   const squareClass = props["squareClass"];
   const onClick = props["onClick"];
@@ -30,48 +34,46 @@ function Square(props) {
 }
 
 // code list of props here, find out how to specify props at bottom
-const Board = (props) => {
-  const array1 = ["a", "b", "c", "d", "e", "f", "g", "h"];
-  const array2 = ["8", "7", "6", "5", "4", "3", "2", "1"];
-  let ids = array2.map((row) => array1.map((col) => col.concat(row)));
+export default class Board extends React.Component {
 
-  let board = ids.map((row) =>
-    row.map((id) => {
-      const col = array1.indexOf(id.charAt(0));
-      const row = array2.indexOf(id.charAt(1));
 
-      const colorClass =
-        (col % 2 === 0 && row % 2 === 0) || (col % 2 === 1 && row % 2 === 1)
-          ? "whiteSquare"
-          : "blackSquare";
-      if (row === 3 || row === 4 || colorClass ==="whiteSquare") {
+  render() {
+    const array1 = ["a", "b", "c", "d", "e", "f", "g", "h"];
+    const array2 = ["8", "7", "6", "5", "4", "3", "2", "1"];
+    let ids = array2.map((row) => array1.map((col) => col.concat(row)));
+
+    let board = ids.map((row) =>
+      row.map((id) => {
+        const col = array1.indexOf(id.charAt(0));
+        const row = array2.indexOf(id.charAt(1));
+
+        const colorClass =
+          (col % 2 === 0 && row % 2 === 0) || (col % 2 === 1 && row % 2 === 1)
+            ? "whiteSquare"
+            : "blackSquare";
+        if (row === 3 || row === 4 || colorClass === "whiteSquare") {
+          return (
+            <Square
+              squareClass={colorClass}
+              onClick={(id) => console.log(id)}
+              id={id}
+            ></Square>
+          );
+        }
+
+        const pieceColor = row > 4 ? redpiece : blackpiece;
+
         return (
           <Square
             squareClass={colorClass}
-            onClick={(id) => console.log(id)}
+            onClick={() => onclick(id)}
             id={id}
+            img={pieceColor}
           ></Square>
         );
-      }
+      })
+    );
 
-      const pieceColor = (row>4) ? redpiece : blackpiece;
-
-      return (
-        <Square
-        squareClass={colorClass}
-        onClick={(id) => console.log(id)}
-        id={id}
-        img={pieceColor}
-      ></Square>
-      )
-    })
-  );
-
-  return (
-    <div className="Container">
-      {board}
-    </div>
-  );
-};
-
-export default Board;
+    return board;
+  }
+}
